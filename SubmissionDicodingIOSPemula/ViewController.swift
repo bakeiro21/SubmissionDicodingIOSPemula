79f0fbc5c208ea682c3872c35301c8c720b0ec4d
Burhan Ahmad <zbrahma21@gmail.com>
2020-05-07 09:07:45 +0700

    Initial Commit

---
+++ SubmissionDicodingIOSPemula/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SubmissionDicodingIOSPemula
+//
+//  Created by Mac Cendana on 07/05/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
